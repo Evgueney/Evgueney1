8f291b0acd834fe7771afffefae3a212f318a71d
Evgueney <perepechko060924@outlook.com>
2024-09-10 21:48:24 +0300

    creat file метод строк

---
+++ метод/метод строк.py
@@ -0,0 +1,10 @@
+#Все способы обозначения ковычек
+Country = "Belarus"
+Town = 'Soligorsk'
+Street = '''Lenina'''
+#Операции сложения и умножения к строкам
+print(Country, Town, Street)
+Fulladress = Country + " " + Town + " " + Street
+print(Fulladress)
+print("Belarus"*10)
+print("Belarus"+"Soligorsk"+"Lenina")
